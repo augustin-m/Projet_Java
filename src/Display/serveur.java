de3790786a883fc1546217bf0d0119dbee30019d
augustin-m <60687546+augustin-m@users.noreply.github.com>
2022-03-24 12:17:46 +0100

    Prise de commande & Affichage du menu

--- src/Display/serveur.java
+++ src/Display/serveur.java
@@ -1,4 +1,35 @@
 package Display;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
 public class serveur {
+    public static List<String> menu = new ArrayList<>(Arrays.asList(
+            "Limonade", "Cidre doux", "Bière sans alcool", "Jus de fruit", "verre d'eau",
+            "Salade", "Salade avec tomates",
+            "Potage aux oignons", "Potage à la tomate", "Potage aux champignons",
+            "Burger avec salade et tomates", "Burger avec salade", "Burger",
+            "Pizza", "Pizza avec champignons", "Pizza avec du chorizo"
+    ));
+    public static List<String> commandePlats = new ArrayList<String>();
+    public static List<String> commandeBoissons = new ArrayList<String>();
+
+    public static void printMenu(){
+        for (int i = 0; i < menu.size();i++){
+            System.out.println(i + " : " + menu.get(i));
+        }
+        System.out.println("16 : FIN DE COMMANDE");
+    }
+    public static void scanCommande(){
+        Scanner scan = new Scanner(System.in);
+        while(scan.nextInt() != 16) {
+            System.out.println(menu.get(scan.nextInt()));
+            if(scan.nextInt() <= 4) commandeBoissons.add(menu.get(scan.nextInt()));
+            else commandePlats.add(menu.get(scan.nextInt()));
+        }
+    }
+
 }
+
