814d5f2963ca255680ecfc5a12cf4e0e96179778
augustin-m <60687546+augustin-m@users.noreply.github.com>
2022-03-31 10:33:07 +0200

    correction bug

--- src/Display/barmanXcuisinier.java
+++ src/Display/barmanXcuisinier.java
@@ -3,6 +3,7 @@ package Display;
 import java.util.Scanner;
 
 public class barmanXcuisinier {
+    public serveur attender;
     public int statut;
     public void print(){
         System.out.println("Etes vous barman ou cuisinier ?");
@@ -11,5 +12,17 @@ public class barmanXcuisinier {
         Scanner scanner = new Scanner(System.in);
         int choixEcran = scanner.nextInt();
         this.statut = choixEcran;
+        if(this.statut == 1){
+            System.out.println("Ecran du Barman :");
+            for (int i = 0; i < attender.commandeBoissons.size();i++){
+                System.out.println(i + " : " + attender.commandeBoissons.get(i));
+            }
+        }
+        else if(this.statut == 2){
+            System.out.println("Ecran du Cuisinier :");
+            for (int i = 0; i < attender.commandePlats.size();i++){
+                System.out.println(i + " : " + attender.commandePlats.get(i));
+            }
+        }
     }
 }
