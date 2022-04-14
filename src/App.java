0081ee67360c9230a96799da8e177bdd1b4e4a47
Lionel SMETS <lionel.smets@isen.yncrea.fr>
2022-04-14 12:00:23 +0200

    Ingrédients se consomment auto

--- src/App.java
+++ src/App.java
@@ -1,9 +1,5 @@
-import management.Serveur;
 import management.Stocks;
 
-import java.io.IOException;
-
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.*;
 
@@ -11,8 +7,6 @@ import java.util.*;
 
 public class App {
     public static void main(String[] args) throws IOException {
-        Stocks test = new Stocks();
-        //test.setStocks();
         menu();
     }
     public static void menu() throws IOException {
@@ -20,7 +14,6 @@ public class App {
         System.out.println("1- Ecran prise de commande");
         System.out.println("2- Ecran cuisine ou de bar");
         System.out.println("3- Ecran Monitoring");
-        Serveur Jack = new Serveur("Jack", "Dubois", 1200);
         Scanner scanner = new Scanner(System.in);
         int choixEcran = scanner.nextInt();
         Affichage Display1 = new Affichage();
--- src/Display/serveur.java
+++ src/Display/serveur.java
@@ -10,33 +10,31 @@ public class serveur {
             "Salade", "Salade avec tomates",
             "Potage aux oignons", "Potage à la tomate", "Potage aux champignons",
             "Burger avec salade et tomates", "Burger avec salade", "Burger",
-            "Pizza", "Pizza avec champignons", "Pizza avec du chorizo"
+            "Pizza", "Pizza avec champignons", "Pizza avec du chorizo", "Fajitas au poulet", "Fajitas au boeuf"
     ));
-    public static List<String> commandePlats = new ArrayList<String>();
-    public static List<String> commandeBoissons = new ArrayList<String>();
-    public static List<String> platsFaits = new ArrayList<String>();
-    public static List<String> boissonsFaites = new ArrayList<String>();
+    public static List<String> commandePlats = new ArrayList<>();
+    public static List<String> commandeBoissons = new ArrayList<>();
+    public static List<String> platsFaits = new ArrayList<>();
+    public static List<String> boissonsFaites = new ArrayList<>();
 
 
     public static void printMenu(){
         for (int i = 0; i < menu.size();i++){
             System.out.println(i + " : " + menu.get(i));
         }
-        System.out.println("16 : FIN DE COMMANDE");
+        System.out.println(menu.size() + " : FIN DE COMMANDE");
     }
     public static void scanCommande(){
         Scanner scan = new Scanner(System.in);
         int verif = scan.nextInt();
-        if(verif < 16) {
+        if(verif < menu.size()) {
             System.out.println(menu.get(verif));
             if(verif <= 4) commandeBoissons.add(menu.get(verif));
             else commandePlats.add(menu.get(verif));
             scanCommande();
         }
-        if(verif == 16){
+        if(verif >= menu.size()){
             System.out.println("Commande envoyée");
-            //TEST : System.out.println("Boissons" + " : " + commandeBoissons);
-            //TEST : System.out.println("Plats" + " : " + commandePlats);
         }
 
     }
